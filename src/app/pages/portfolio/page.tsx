590bcf01a244584a13d45c10fd83760307b46e92
brian-dev01 <brian147promoth@gmail.com>
2025-02-06 05:39:01 +0530

    API

---
+++ src/app/pages/portfolio/page.tsx
@@ -0,0 +1,10 @@
+import PortfolioSection from "@/components/sections/PortfolioSection";
+
+export default function PortfolioPage() {
+    return (
+      <div className="px-4 md:px-[6rem] mt-[5rem]">
+        <PortfolioSection />
+      </div>
+    );
+  }
+  
---
+++ src/app/pages/services/page.tsx
@@ -0,0 +1,10 @@
+import ServiceSection from "@/components/sections/ServiceSection";
+
+export default function ServicesPage() {
+    return (
+      <div className="px-4 md:px-[6rem] mt-[5rem]">
+        <ServiceSection />
+      </div>
+    );
+  }
+  
